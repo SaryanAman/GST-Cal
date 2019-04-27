d10f981ce58a0dbc37fec328dc60d7fd9124d60e
AMAN SINGH <37333921+saryan720@users.noreply.github.com>
2019-04-27 13:39:08 +0530

    Add files via upload
    use it

---
+++ divyanshugst.py
@@ -0,0 +1,14 @@
+a = float(input('enter amount = '))
+b = a/100*9
+c = b+b
+d = a+c
+print('amount = ',a,'Rs')
+print('CGST charged (9%) = ',b,'Rs')
+print('SGST charged (9%) = ',b,'Rs')
+print ('Total tax = ',c,'Rs')
+print ("Total amount = " , d,'Rs')
+input('press any key to exit')
+      
+      
+      
+        
